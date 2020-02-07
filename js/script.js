070926f6831522e423a767316e2366df5e35d549
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-02-07 17:37:58 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -26,12 +26,13 @@ $(document).ready(function(){
 // FUNZIONI-----------
   function stampaFilm(films){
     $('.listaFilm').html('');
-
     var source = $('#film-template').html();
     var template = Handlebars.compile(source);
   for (var i = 0; i < films.length; i++) {
     var thisFilm = films[i];
 
+
+// FUNZIONE PRINTSTARS
 function printStars(){
 
   // VOTO IN NUMERO DA 1 A 5
@@ -63,19 +64,33 @@ function printStars(){
   }
   return stars;
 }
+// FINE FUNZIONE PRINTSTARS
+// FUNZIONE BANDIERA
+function flag(){
+  var nation = thisFilm.original_language;
+  if(nation == 'en'){
+    var bandiera = '<img src="https://www.pontemagra.com/open2b/var/products/10/87/0-f40a2c47-500-Bandiera-Gran-Bretagna.jpg" alt="en">'
+  }else if(nation == 'de'){
+    var bandiera = '<img src="https://www.germania.xyz/wp-content/uploads/2017/12/bandierade.jpg" alt="de">'
+  }else{
+    bandiera = nation;
+  }
+  return bandiera;
+}
 
     var context = {
       title: thisFilm.title,
       original_title: thisFilm.original_title,
-      original_language: thisFilm.original_language,
+      original_language: flag(thisFilm.original_language),
       vote_average: printStars(thisFilm.vote_average)
      };
 
     var html = template(context);
     $('.listaFilm').append(html);
+    }
   }
 
-}
+
 
 
 
